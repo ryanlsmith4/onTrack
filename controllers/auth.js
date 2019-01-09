e81bb4c4468eabefc22cc3515120c0903bea6a40
Ryan Smith <ryan.l.smith4@gmail.com>
2019-01-08 16:10:08 -0800

    added views for login and sign up with routes to buttons

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,6 +5,18 @@ const router = express.Router()
 
 router.get('/sign-up', (req, res) => {
     res.render('sign-up');
-})
+});
+
+router.get('/sign-up/manager', (req, res) => {
+    res.render('managers');
+});
+
+router.get('/sign-up/employee', (req, res) => {
+    res.render('employees');
+});
+
+router.get('/log-in', (req, res) => {
+    res.render('log-in');
+});
 
 module.exports = router;
